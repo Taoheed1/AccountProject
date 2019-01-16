69e1e9a6296737628b4428b037dcd6450624a59f
Taoheed Afolayan <taoheed123afolayan@gmail.com>
2019-01-16 10:41:25 +0000

    Tdd feature branch - first name search

--- src/test/java/com/qa/tests/PersistanceTests.java
+++ src/test/java/com/qa/tests/PersistanceTests.java
@@ -46,6 +46,43 @@ public class PersistanceTests {
 		service.addAccount("Chris", "Bowers");
 		assertEquals(1, service.getAccount(1).getAccountNumber());
 	}
+	
+	@Test
+	public void firstNameSearchTest() {
+		
+		service.addAccount("Jeff", "Jeffers");
+		service.addAccount("Jeff", "Bowers");
+		service.addAccount("Jeff", "Jeff");
+
+		assertEquals(3,service.firstNameSearch("Jeff"));
+	}
+
+	@Test
+	public void firstNameSearchTest2() {
+		
+		service.addAccount("Steph", "Jeffers");
+		service.addAccount("Jeff", "Bowers");
+		service.addAccount("Jeff", "Jeff");
+
+		assertEquals(2,service.firstNameSearch("Jeff"));
+	}
+	
+	@Test 
+	public void lastNameSearchTest() {
+		service.addAccount("Mick", "Jenkins");
+		service.addAccount("Leeroy", "Jenkins");
+		service.addAccount("Jennifer", "Jenkins");
+		
+		assertEquals(3, service.lastNameSearch("Jenkins"));
+	}
+	@Test 
+	public void lastNameSearchTest2() {
+		service.addAccount("Mick", "Jenkins");
+		service.addAccount("Leeroy", "Jenkins");
+		service.addAccount("Jennifer", "Lawrence");
+		
+		assertEquals(2, service.lastNameSearch("Jenkins"));
+	}
 
 	
 	
